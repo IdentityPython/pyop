dd82340a28d516a80cb7500f72f1ae3b45b2b11f
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-10-20 11:53:54 +0200

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/its-dirg/pyop',
