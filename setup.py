6272ce70211f42b99bc6dd9ce19431530379af47
Johan Lundberg <lundberg@sunet.se>
2017-08-01 12:46:39 +0200

    Bump version: 2.0.5 → 2.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='2.0.5',
+    version='2.0.6',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/its-dirg/pyop',
