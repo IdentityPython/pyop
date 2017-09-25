cf57877a5e620cbf90d8f861f81bb17ee1271c4d
John Van de Meulebrouck Brendgard <john@sunet.se>
2017-09-25 13:55:41 +0200

    Bump version: 2.0.6 → 2.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='2.0.6',
+    version='2.0.7',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/SUNET/pyop',
