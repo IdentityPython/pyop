f94bf5507231eaf646a701c7661fabd331719cd8
Johan Lundberg <lundberg@sunet.se>
2017-10-16 10:48:28 +0200

    Bump version: 2.0.7 → 2.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='2.0.7',
+    version='2.0.8',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/SUNET/pyop',
