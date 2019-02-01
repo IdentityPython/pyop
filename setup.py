7b1385964f079c39752fce5f2dbcf458b8a92e56
Ivan Kanakarakis <ivan.kanak@gmail.com>
2019-02-02 01:03:39 +0200

    Bump version to 3.0.1
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='3.0.0',
+    version='3.0.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/IdentityPython/pyop',
