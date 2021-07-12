0ba83aa3dbac4226171bbfebdd4d584f724db637
Ivan Kanakarakis <ivan.kanak@gmail.com>
2021-07-13 02:37:15 +0300

    Bump version to 3.2.0
    - Add Redis storage backend
    - Add automatic expiry to MongoDB
    - Declare Mongo and Redis as extra requires
    
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='3.1.1',
+    version='3.2.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/IdentityPython/pyop',
