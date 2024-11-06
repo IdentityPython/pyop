fab87f9f6193079171fdad0c223c810fe9532dd2
Ivan Kanakarakis <ivan.kanak@gmail.com>
2024-11-06 13:16:15 +0200

    Bump version to v3.4.2
    For the stateless flow, retrieve the extra_id_token_claims from the auth_code in the request
    
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyop',
-    version='3.4.1',
+    version='3.4.2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/IdentityPython/pyop',
