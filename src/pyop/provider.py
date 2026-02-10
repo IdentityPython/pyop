3e52b8e0158686b769b0987eefc7266eea495da4
Benjamin Truninger <benjamin.truninger@dieboldnixdorf.com>
2026-02-10 16:21:39 +0100

    Do not overwrite extra_id_token_claims if it is already set

--- src/pyop/provider.py
+++ src/pyop/provider.py
@@ -446,7 +446,9 @@ class Provider(object):
         if refresh_token is not None:
             response['refresh_token'] = refresh_token
 
-        extra_id_token_claims = {}
+        if extra_id_token_claims is None:
+            extra_id_token_claims = {}
+
         if self.stateless:
             extra_id_token_claims_in_code = self.authz_state.get_extra_id_token_claims_for_code(token_request['code'])
             extra_id_token_claims.update(extra_id_token_claims_in_code)
